90e1a8ba84ee764232235159812dae3666e146e3
rominarg <rominagnavarrete@gmail.com>
2023-03-09 17:26:44 -0300

    first commit

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,29 +1,130 @@
-const cardContainer = document.getElementById('cardContainer')
+// Obtengo los datos del objeto 'data'
+let dataOriginalEvents = data.events;
+// Obtengo los elementos del DOM necesarios
+let cardContainer = document.getElementById ("cardContainer")
+let checkContainer = document.getElementById ("checkContainer")
 
-for (let i = 0; i<data.events.length; i++){
-
-    let card = document.createElement('section');
-    card.innerHTML = `
-    <div class="container">
-        <div class="row p-2 justify-content-center">
-        <div class="col-md-3" style="width: 18rem; height: 30rem;">
-            <div class="card">
-            <img src="${data.events[i].image}" class="card-img-top" alt="...">
-            <div class="card-body" style="height: auto;">
-                <h5 class="card-title fw-bold">${data.events[i].name}</h5>
-                <p class="card-text">${data.events[i].description}</p>
-                <p class="card-text m-0"><span class="bold-color-card">Place: </span> ${data.events[i].place}</p>
-                <p class="card-text m-0"><span class="bold-color-card">Capacity: </span> ${data.events[i].capacity}</p>
-                <p class="card-text m-0"><span class="bold-color-card">Assistance: </span>${data.events[i].assistance}</p>
-                <div class="d-flex p-2 justify-content-between h-10 ">
-                <p class="card-price m-0"><strong>Price $${data.events[i].price}</strong></p>
-                <a href="./details.html" class="btn btn-primary">See More</a>
-                </div>
+// Función que imprime las cards
+function imprimirCard(array) {
+  let locationforCards = "";
+  if (array.length !== 0) {
+    array.forEach((data) => {
+      locationforCards +=`
+      <div class="col-md-3 col-sm-6 col-md-4 col-lg-3">
+        <div class="card">
+          <img src="${data.image}"class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title fw-bold">${data.name}</h5>
+            <p class="card-text">${data.description}</p>
+            <div class="d-flex p-2 justify-content-between h-10 ">
+              <p class="card-price m-0">Price$${data.price}</p>
+              <a href="./details.html?id=${data._id}" class="btn btn-primary">See More</a>
             </div>
-            </div>
+          </div>
         </div>
-        </div>
-    </div>
+      </div>
+      `;
+    });
+    // Agrego las tarjetas al contenedor en el DOM
+    cardContainer.innerHTML = `
+      <div class="row justify-content-center text-center">
+        ${locationforCards}
+      </div>
     `;
-    cardContainer.appendChild(card)
-}
+    // Si no hay eventos, mostramos un mensaje
+  } else {
+    cardContainer.innerHTML = `
+      <div class="d-flex align-items-center justify-content-center">
+        <div class="text-center">
+          <img src="./assets/img/not_found.png" alt="events_not _found" class="logo d-inline-block align-text-top">
+        </div>
+      </div>
+      <section class="button_back">
+      <a href="index.html" class="btn btn-primary button_back">Back</button>
+      </section>
+      `;
+    }
+  }
+  imprimirCard(dataOriginalEvents);
+
+
+// Obtenemos las categorías de los eventos y creamos los checkboxes correspondientes
+  let checkboxsOrigin = dataOriginalEvents.map((evento) => evento.category);
+  let filterrepeat = new Set(checkboxsOrigin); 
+  let categoriesCheck = [...filterrepeat];
+
+// Función que imprime los checkboxes
+  function impCheck() {
+    let impCheckboxslocation = "";
+    categoriesCheck.forEach((categoria) => {
+      impCheckboxslocation += `<label class="checks_search">            
+      <input class="form-check-input text-start" type="checkbox" 
+      value=${categoria} id="flexCheckChecked">
+      ${categoria}
+      </label>`;
+    });
+    checkContainer.innerHTML = impCheckboxslocation;
+  }
+  impCheck();
+
+ // Listener de eventos para los checkboxes
+  let checkboxSelected = [];
+  let textSearch = "";
+  let checkbox = document.querySelectorAll("input[type=checkbox]");
+    checkbox.forEach((check) =>
+      check.addEventListener("click", (event) => {
+        let checked = event.target.checked;
+        if (checked) {
+// Si se selecciona un checkbox, lo agregamos al array de categorías seleccionadas y actualizamos las cards
+          checkboxSelected.push(event.target.value);
+          crossfilterDoble();
+        } else {
+ // Si se deselecciona un checkbox, lo eliminamos del array de categorías seleccionadas y actualizamos las cards
+          checkboxSelected = checkboxSelected.filter(
+            (uncheck) => uncheck !== event.target.value
+          );
+          crossfilterDoble();
+        }
+      })
+    );
+
+//Search, captura y evento
+  let search = document.getElementById("searchLocation");
+  search.addEventListener("keyup", (event) => {
+    textSearch = event.target.value;
+    crossfilterDoble();
+  });
+
+  //Filtro cruzado
+  function crossfilterDoble() {
+    let datos = [];
+    if (checkboxSelected.length > 0 && textSearch !== "") {
+      checkboxSelected.map((selected) => {
+        datos.push(...dataOriginalEvents.filter((evento) =>
+              evento.name.toLocaleLowerCase().includes(
+                textSearch.trim().toLocaleLowerCase()
+              ) && evento.category.includes(selected)
+          )
+        );
+
+      });
+    } else if (checkboxSelected.length > 0 && textSearch === "") {
+      checkboxSelected.map((selected) => {
+        datos.push(
+          ...dataOriginalEvents.filter((evento) => evento.category.includes(selected))
+        );
+      });
+    } else if (checkboxSelected.length == 0 && textSearch !== "") {
+      datos.push(
+        ...dataOriginalEvents.filter((evento) =>
+          evento.name
+            .toLocaleLowerCase()
+            .includes(textSearch.trim().toLocaleLowerCase())
+        )
+      );
+    } else {
+      datos.push(...dataOriginalEvents);
+    }
+    imprimirCard(datos);
+  }
+  crossfilterDoble();
--- Assets/js/past.js
+++ Assets/js/past.js
@@ -1,34 +1,126 @@
-let cardContainer = document.getElementById('cardContainer');
-let currentDateData = parseInt(data.currentDate);
+let dataOriginalEvents = data.events;
+let curDate = data.currentDate;
+let cardContainer = document.getElementById ("cardContainer")
+let checkContainer = document.getElementById ("checkContainer")
 
-for (let i = 0; i < data.events.length; i++) {
-  if (data.events[i].date < data.currentDate) {
-
-    let card = document.createElement('section');
-    card.innerHTML = `
-    <div class="container">
-        <div class="row p-2 justify-content-center">
-        <div class="col-md-3" style="width: 18rem; height: 30rem;">
-            <div class="card">
-            <img src="${data.events[i].image}" class="card-img-top" alt="...">
-            <div class="card-body" style="height: auto;">
-                <h5 class="card-title fw-bold">${data.events[i].name}</h5>
-                <p class="card-text">${data.events[i].description}</p>
-                <p class="card-text m-0"><span class="bold-color-card">Place: </span> ${data.events[i].place}</p>
-                <p class="card-text m-0"><span class="bold-color-card">Capacity: </span> ${data.events[i].capacity}</p>
-                <p class="card-text m-0"><span class="bold-color-card">Assistance: </span>${data.events[i].assistance}</p>
-                <div class="d-flex p-2 justify-content-between h-10 ">
-                <p class="card-price m-0"><strong>Price $${data.events[i].price}</strong></p>
-                <a href="./details.html" class="btn btn-primary">See More</a>
-                </div>
+function imprimirCardPast(array) {
+  let templateHtml = "";
+  if (array.length !== 0) {
+    array.forEach((data) => {
+      if (data.date < curDate) {
+        templateHtml +=`
+        <div class="col-md-3 col-sm-6 col-md-4 col-lg-3 mx-4 my-1">
+          <div class="card">
+            <img src="${data.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title fw-bold">${data.name}</h5>
+              <p class="card-text">${data.description}</p>
+              <div class="d-flex p-3 justify-content-between h-20">
+                <p class="card-price m-0">Price $${data.price}</p>
+                <a href="./details.html?id=${data._id}" class="btn btn-primary">See More</a>
+              </div>
             </div>
+          </div>
+        </div>`;
+      }
+      cardContainer.innerHTML = `
+        <section>
+          <div class="container">
+            <div class="row justify-content-center text-center">
+              ${templateHtml}
             </div>
-        </div>
-        </div>
+          </div>
+        </section>`;
+    });
+  } else {
+    cardContainer.innerHTML = `
+    <div class="d-flex align-items-center justify-content-center">
+    <div class="text-center">
+    <img src="./assets/img/not_found.png" alt="events_not _found" class="logo d-inline-block align-text-top">
     </div>
-    `;
-    cardContainer.appendChild(card);
+  </div>
+  <section class="button_back">
+  <a href="past_events.html" class="btn btn-primary button_back">Back</button>
+  </section>
+  `;
   }
 }
+imprimirCardPast(dataOriginalEvents);
 
+let checkboxsOrigin = dataOriginalEvents.map((evento) => evento.category);
+let filterrepeat = new Set(checkboxsOrigin); 
+let categoriesCheck = [...filterrepeat];
 
+function impCheck() {
+  let impCheckboxslocation = "";
+  categoriesCheck.forEach((categoria) => {
+    impCheckboxslocation += `<label class="checks_search">            
+    <input class="form-check-input text-start" type="checkbox" 
+    value=${categoria} id="flexCheckChecked">
+    ${categoria}
+    </label>`;
+  });
+  checkContainer.innerHTML = impCheckboxslocation;
+}
+
+impCheck();
+
+let checkboxSelected = [];
+let textSearch = "";
+let checkbox = document.querySelectorAll("input[type=checkbox]");
+  checkbox.forEach((check) =>
+    check.addEventListener("click", (event) => {
+      let checked = event.target.checked;
+
+      if (checked) {
+
+        checkboxSelected.push(event.target.value);
+        crossfilterDoble();
+
+      } else {
+        checkboxSelected = checkboxSelected.filter(
+          (uncheck) => uncheck !== event.target.value
+        );
+        crossfilterDoble();
+      }
+    })
+  );
+
+let search = document.getElementById("searchLocation");
+search.addEventListener("keyup", (event) => {
+  textSearch = event.target.value;
+  crossfilterDoble();
+});
+
+function crossfilterDoble() {
+  let datos = [];
+  if (checkboxSelected.length > 0 && textSearch !== "") {
+    checkboxSelected.map((selected) => {
+      datos.push(...dataOriginalEvents.filter((evento) =>
+            evento.name.toLocaleLowerCase().includes(
+              textSearch.trim().toLocaleLowerCase()
+            ) && evento.category.includes(selected)
+        )
+      );
+
+    });
+  } else if (checkboxSelected.length > 0 && textSearch === "") {
+    checkboxSelected.map((selected) => {
+      datos.push(
+        ...dataOriginalEvents.filter((evento) => evento.category.includes(selected))
+      );
+    });
+  } else if (checkboxSelected.length == 0 && textSearch !== "") {
+    datos.push(
+      ...dataOriginalEvents.filter((evento) =>
+        evento.name
+          .toLocaleLowerCase()
+          .includes(textSearch.trim().toLocaleLowerCase())
+      )
+    );
+  } else {
+    datos.push(...dataOriginalEvents);
+  }
+  imprimirCardPast(datos);
+}
+crossfilterDoble();
